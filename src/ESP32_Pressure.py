aac2e177fe96d374294b274ca0665ddbcda0e7c9
Gia JEONG <95063848+gladgaj@users.noreply.github.com>
2025-08-14 16:51:29 -0700

    updated msg type/checked working

--- src/ESP32_Pressure.py
+++ src/ESP32_Pressure.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
-
-## ESP32-S3 feather pressure sensor reading and publish import rospy
-
+## ESP32-S3 feather pressure sensor reading and publish
 import rospy
 import numpy as np
 import os, sys
@@ -12,7 +10,7 @@ from pushpull_suctioncup_106a.msg import cmdPacket
 import serial
 from serial import Serial
 import struct
-
+import argparse
 
 IDLE = 0
 STREAMING = 1
@@ -24,63 +22,59 @@ IDLE_CMD = 3
 currState = IDLE
 CMD_in = NO_CMD
 
-
 def callback(data):
     global CMD_in        
     CMD_in = data.cmdInput
 
-
-def main():
+def main(args):
     global currState
     global CMD_in
 
     rospy.init_node('ESP32_Pressure')
-    print("cuurent state: ", currState)
-    print("CMD_in: ", CMD_in)
-    #Sensor reading is published to topic 'SensorPacket'
+    rospy.loginfo("Num of channels (args.ch): %d", args.ch)
+
+    # Sensor reading is published to topic 'SensorPacket'
     pub = rospy.Publisher('SensorPacket', SensorPacket, queue_size=10)
-    rospy.Subscriber("cmdPacket",cmdPacket, callback)
+    rospy.Subscriber("cmdPacket", cmdPacket, callback)
 
-    print("cuurent state: ", currState)
-    print("CMD_in: ", CMD_in)
     msg = SensorPacket()
-    msg.data = [0.0, 0.0, 0.0, 0.0] 
+    msg.ch = args.ch                          # ch 
+    msg.data = [0.0] * args.ch                 
 
-    # ser = serial.Serial("/dev/ttyACM0", baudrate=115200, timeout=1, write_timeout=1)
+    # Serial communication setup
     ser = serial.Serial("/dev/ttyPressure", baudrate=115200, timeout=1, write_timeout=1)
     ser.flushInput()
- 
+
     while not rospy.is_shutdown():
         try:
             CMD_in = START_CMD
             if currState == IDLE and CMD_in == START_CMD:
-                print("true")
+                rospy.loginfo("Streaming start")
                 CMD_in = NO_CMD
                 ser.write(struct.pack('<B', ord("i")))
                 rospy.sleep(0.01)
                 ser.write(struct.pack('<B', ord("s")))
                 rospy.sleep(0.01)
-                while not CMD_in == IDLE_CMD and not rospy.is_shutdown():
-                    
-                    ser_bytes = ser.readline().decode("utf-8")
-                    # print("ser_bytes: ", ser_bytes)
-                    split_data = ser_bytes.split(' ')
-                    # print("sizeof_split_data: ", len(split_data))
-                    first_val = split_data[0]
-                    # print("first_vale: ", first_val)
-                    second_val = split_data[1]
-                    third_val = split_data[2]
-                    fourth_val = split_data[3]
-                    msg.data[0] = float(first_val)
-                    msg.data[1] = float(second_val)
-                    msg.data[2] = float(third_val)
-                    msg.data[3] = float(fourth_val)
-                    print(msg)
-                    msg.header.stamp = rospy.Time.now()
-                    print("test")
-                    pub.publish(msg)
 
-                # ser.write("i" + "\r\n")
+                while not CMD_in == IDLE_CMD and not rospy.is_shutdown():
+                    ser_bytes = ser.readline().decode("utf-8").strip()
+                    split_data = ser_bytes.split(' ')
+
+                    if len(split_data) < args.ch:
+                        rospy.logwarn("Received fewer data points than expected. Got %d, expected %d",
+                                      len(split_data), args.ch)
+
+                        continue
+
+                    # publishing sensor data
+                    for i in range(args.ch):
+                        msg.data[i] = float(split_data[i])
+
+                    msg.header.stamp = rospy.Time.now()
+                    pub.publish(msg)
+                    print(msg)
+
+                # STOP signal
                 ser.write(struct.pack('<B', ord("i")))
                 ser.flushInput()
                 rospy.sleep(0.01)            
@@ -88,15 +82,16 @@ def main():
                 currState = IDLE
         
         except Exception as e:
-            print("SensorComError: " + str(e))
+            rospy.logerr("SensorComError: " + str(e))
             pass
-    print("ESP32 Sensor Reading Done")
+
+    rospy.loginfo("ESP32 Sensor Reading Done")
 
 if __name__ == '__main__':
     try:
-        print("Started!")
-        main()
-
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--ch', type=int, help='number of channel', default=4)
+        args = parser.parse_args()    
+        main(args)
     except rospy.ROSInterruptException: 
-        print("oops")
         pass
